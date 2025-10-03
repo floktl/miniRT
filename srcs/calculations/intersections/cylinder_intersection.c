57a2cd0b470bc642f60d9ee72fe56284cfb35ba9
Mengyao Zhang <mezhang@3-A-12.42heilbronn.de>
2025-10-03 17:54:06 +0200

    cylinder: fixed bugs for its caps. cone: added cap

--- srcs/calculations/intersections/cylinder_intersection.c
+++ srcs/calculations/intersections/cylinder_intersection.c
@@ -6,7 +6,7 @@
 /*   By: mezhang <mezhang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/20 11:00:00 by fkeitel           #+#    #+#             */
-/*   Updated: 2025/10/03 14:09:08 by mezhang          ###   ########.fr       */
+/*   Updated: 2025/10/03 17:23:13 by mezhang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,8 @@ static double	calculate_quadratic_terms(t_vec3d ray_dir, t_vec3d oc,
 	t_vec3d	axis_perp;
 	double	dot_oc_axis;
 	double	dot_oc_dir;
-	double params[3]; // a, b, c
 
+	double params[3]; // a, b, c
 	axis_perp = vec_sub(ray_dir, vec_mul(obj->data.s_cylinder.axis,
 				dot_dir_axis));
 	dot_oc_axis = vec_dot(oc, obj->data.s_cylinder.axis);
@@ -37,8 +37,7 @@ static double	calculate_quadratic_terms(t_vec3d ray_dir, t_vec3d oc,
 	return (params[1] * params[1] - 4 * params[0] * params[2]);
 }
 
-static bool	is_intersection_within_cylinder(t_ray ray, t_object *obj, double t,
-		double dot_dir_axis)
+static bool	is_intersection_inside(t_ray ray, t_object *obj, double t)
 {
 	t_vec3d	p;
 	double	m;
@@ -51,14 +50,34 @@ static bool	is_intersection_within_cylinder(t_ray ray, t_object *obj, double t,
 	return (true);
 }
 
-static double	find_cylinder_intersection(t_ray ray, t_object *obj,
+static bool	is_intersection_on_cap(t_ray ray, t_object *obj, double t, int cap)
+{
+	t_vec3d	p;
+	t_vec3d	v;
+	double	dist;
+
+	p = vec_add(ray.origin, vec_mul(ray.direction, t));
+	if (cap == 0)
+		v = vec_sub(p, obj->data.s_cylinder.base);
+	else
+		v = vec_sub(p, vec_add(obj->data.s_cylinder.base,
+					vec_mul(obj->data.s_cylinder.axis,
+						obj->data.s_cylinder.height)));
+	dist = vec_length(vec_sub(v, vec_mul(obj->data.s_cylinder.axis, vec_dot(v,
+						obj->data.s_cylinder.axis))));
+	if (dist > obj->data.s_cylinder.radius)
+		return (false);
+	return (true);
+}
+
+static double	find_cylinder_side_intersection(t_ray ray, t_object *obj,
 		double dot_dir_axis, double disc)
 {
 	double	t[2];
 	double	dot_oc_axis;
 	double	dot_oc_dir;
-	double params[2]; // a, b, c
 
+	double params[2]; // a, b, c
 	dot_oc_dir = vec_dot(vec_sub(ray.origin, obj->data.s_cylinder.base),
 			ray.direction);
 	dot_oc_axis = vec_dot(vec_sub(ray.origin, obj->data.s_cylinder.base),
@@ -67,21 +86,23 @@ static double	find_cylinder_intersection(t_ray ray, t_object *obj,
 	params[1] = 2 * (dot_oc_dir - dot_oc_axis * dot_dir_axis);
 	t[0] = (-params[1] - sqrt(disc)) / (2 * params[0]);
 	t[1] = (-params[1] + sqrt(disc)) / (2 * params[0]);
-	if (t[0] > 0 && is_intersection_within_cylinder(ray, obj, t[0], dot_dir_axis))
+	if (t[0] > 0 && is_intersection_inside(ray, obj, t[0]))
 		return (t[0]);
-	if (t[1] > 0 && is_intersection_within_cylinder(ray, obj, t[1], dot_dir_axis))
+	if (t[1] > 0 && is_intersection_inside(ray, obj, t[1]))
 		return (t[1]);
 	return (-1.0);
 }
 
-static double	calculate_cap_intersection(t_ray ray, t_object *obj,
+static double	find_cylinder_cap_intersection(t_ray ray, t_object *obj,
 		double dot_dir_axis)
 {
 	double	t1;
 	double	t2;
 	double	denom;
+	double	res;
 
 	denom = dot_dir_axis;
+	res = -1.0;
 	if (fabs(denom) > 1e-6)
 	{
 		t1 = (vec_dot(obj->data.s_cylinder.base, obj->data.s_cylinder.axis)
@@ -89,15 +110,14 @@ static double	calculate_cap_intersection(t_ray ray, t_object *obj,
 		t2 = (vec_dot(vec_add(obj->data.s_cylinder.base,
 						vec_mul(obj->data.s_cylinder.axis,
 							obj->data.s_cylinder.height)),
-					obj->data.s_cylinder.axis)
-				- vec_dot(ray.origin, obj->data.s_cylinder.axis)) / denom;
-		if (!is_intersection_within_cylinder(ray, obj, t1, dot_dir_axis) || 
-			!is_intersection_within_cylinder(ray, obj, t2, dot_dir_axis))
-			return (-1.0);
-		if (t1 > 0.0 && t2 > 0.0)
-			return (fmin(t1, t2));
+					obj->data.s_cylinder.axis) - vec_dot(ray.origin,
+					obj->data.s_cylinder.axis)) / denom;
+		if (t1 > 0 && is_intersection_on_cap(ray, obj, t1, 0))
+			res = t1;
+		else if (t2 > 0 && is_intersection_on_cap(ray, obj, t2, 1))
+			res = fmin(res, t2);
 	}
-	return (-1.0);
+	return (res);
 }
 
 double	intersect_cylinder(t_ray ray, t_object *obj)
@@ -105,15 +125,20 @@ double	intersect_cylinder(t_ray ray, t_object *obj)
 	t_vec3d	oc;
 	double	dot_dir_axis;
 	double	disc;
-	double	t;
+	double	t_side;
+	double	t_cap;
 
 	oc = vec_sub(ray.origin, obj->data.s_cylinder.base);
 	dot_dir_axis = vec_dot(ray.direction, obj->data.s_cylinder.axis);
-	t = calculate_cap_intersection(ray, obj, dot_dir_axis);
-	if (t > 0.0)
-		return (t);
 	disc = calculate_quadratic_terms(ray.direction, oc, obj, dot_dir_axis);
 	if (disc < 0)
-		return (-1.0);
-	return (find_cylinder_intersection(ray, obj, dot_dir_axis, disc));
+		t_side = -1.0;
+	else
+		t_side = find_cylinder_side_intersection(ray, obj, dot_dir_axis, disc);
+	t_cap = find_cylinder_cap_intersection(ray, obj, dot_dir_axis);
+	if (t_side > 0.0 && (t_cap < 0.0 || t_side < t_cap))
+		return (t_side);
+	if (t_cap > 0.0)
+		return (t_cap);
+	return (-1.0);
 }
--- srcs/calculations/intersections/intersection_utils.c
+++ srcs/calculations/intersections/intersection_utils.c
@@ -6,7 +6,7 @@
 /*   By: mezhang <mezhang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/20 11:00:00 by fkeitel           #+#    #+#             */
-/*   Updated: 2025/10/03 12:52:09 by mezhang          ###   ########.fr       */
+/*   Updated: 2025/10/03 17:49:24 by mezhang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,6 +48,32 @@ double	find_closest_intersection(t_ray ray, t_scene *scene, t_object **hit_obj)
 	return (closest_dist);
 }
 
+static t_vec3d	get_cylinder_normal(t_object *obj, t_vec3d point)
+{
+	t_vec3d	v;
+	t_vec3d	perp;
+	double	m;
+
+	v = vec_sub(point, obj->data.s_cylinder.base);
+	m = vec_dot(v, obj->data.s_cylinder.axis);
+	perp = vec_sub(v, vec_mul(obj->data.s_cylinder.axis, m));
+	return (vec_normalize(perp));	
+}
+
+static t_vec3d	get_cone_normal(t_object *obj, t_vec3d point)
+{
+	t_vec3d	v;
+	t_vec3d	scaled_axis;
+	double	m;
+	double	k;
+
+	v = vec_sub(point, obj->data.s_cone.vertex);
+	m = vec_dot(v, obj->data.s_cone.axis);
+	k = obj->data.s_cone.radius / obj->data.s_cone.height;
+	scaled_axis = vec_mul(obj->data.s_cone.axis, m * (1 + k * k));
+	return (vec_normalize(vec_sub(v, scaled_axis)));
+}
+
 t_vec3d	get_normal(t_vec3d point, t_object *obj)
 {
 	t_vec3d	normal;
@@ -57,9 +83,9 @@ t_vec3d	get_normal(t_vec3d point, t_object *obj)
 	else if (obj->type == PLANE)
 		normal = obj->data.s_plane.normal;
 	else if (obj->type == CYLINDER)
-		normal = vec_normalize(vec_sub(point, obj->data.s_cylinder.base));
+		normal = get_cylinder_normal(obj, point);
 	else if (obj->type == CONE)
-		normal = vec_normalize(vec_sub(point, obj->data.s_cone.vertex));
+		normal = get_cone_normal(obj, point);
 	else
 		normal = (t_vec3d){0, 0, 1};
 	return (normal);
